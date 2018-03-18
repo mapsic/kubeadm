f09a9a37dd9d021cafa32618a330775f1b1eb507
mapsic <mapsic@163.com>
2018-03-18 18:34:56 +0800

    Update downlad_images.sh

--- downlad_images.sh
+++ downlad_images.sh
@@ -8,17 +8,14 @@ KUBE_PAUSE_VERSION=3.0
 ETCD_VERSION=3.1.11
 DNS_VERSION=1.14.7
 
-#GCR_URL=gcr.io/google_containers
-GCR_URL=k8s.gcr.io
+GCR_URL=gcr.io/google_containers
 ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/mapsic
 
 images=("kube-proxy-amd64:${KUBE_VERSION}" "kube-scheduler-amd64:${KUBE_VERSION}" "kube-controller-manager-amd64:${KUBE_VERSION}" "kube-apiserver-amd64:${KUBE_VERSION}" "pause-amd64:${KUBE_PAUSE_VERSION}" "etcd-amd64:${ETCD_VERSION}" "k8s-dns-sidecar-amd64:${DNS_VERSION}"
  "k8s-dns-kube-dns-amd64:${DNS_VERSION}" "k8s-dns-dnsmasq-nanny-amd64:${DNS_VERSION}" )
-
 
 for imageName in ${images[@]} ; do
   docker pull $ALIYUN_URL/$imageName
   docker tag  $ALIYUN_URL/$imageName $GCR_URL/$imageName
   docker rmi  $ALIYUN_URL/$imageName
 done
-
