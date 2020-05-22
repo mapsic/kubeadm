44eb5a9316d7b745783ea822f9a29eb1becee201
mapsic <amkcmd@gmail.com>
2020-05-22 17:44:20 +0800

    Update download_images.sh

--- download_images.sh
+++ download_images.sh
@@ -9,7 +9,7 @@ ETCD_VERSION=3.1.12
 DNS_VERSION=1.14.8
 
 GCR_URL=gcr.io/google_containers
-ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/mapsic
+ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/google_containers
 
 images=("kube-proxy-amd64:${KUBE_VERSION}" "kube-scheduler-amd64:${KUBE_VERSION}" "kube-controller-manager-amd64:${KUBE_VERSION}" "kube-apiserver-amd64:${KUBE_VERSION}" "pause-amd64:${KUBE_PAUSE_VERSION}" "etcd-amd64:${ETCD_VERSION}" "k8s-dns-sidecar-amd64:${DNS_VERSION}"
  "k8s-dns-kube-dns-amd64:${DNS_VERSION}" "k8s-dns-dnsmasq-nanny-amd64:${DNS_VERSION}" )
